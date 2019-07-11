9c7e8f24676325fa2f5a91ba629d18c18c72c9eb
See Xiong <seexiong@Sees-MacBook-Air.local>
2019-07-11 03:48:55 -0500

    side nav bar

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    // Initialization for Slider from materialize
+    $('.slider').slider({
+        full_width: true,
+        height : 700, // default - height : 400
+        interval: 4000, // default - interval: 6000
+    });
+       
+    
+    // Initialization for side nav bar from materialize
+    $('.sidenav').sidenav();
+});
